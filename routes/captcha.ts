635828d905fbca9125629cea736fad90bf10ad1c
Jacek <jacek.poreda@sonarsource.com>
2025-06-17 11:05:34 +0200

    Update captcha.ts

--- routes/captcha.ts
+++ routes/captcha.ts
@@ -62,6 +62,18 @@ export function captchas () {
     true();
 
 
+
+
+    true();
+    true();
+    true();
+
+
+    true();
+    true();
+    true();
+
+
     
     const captchaInstance = CaptchaModel.build(captcha)
     await captchaInstance.save()
